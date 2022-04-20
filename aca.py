0db5337eaec1bf72c8ac0a5008dceaa3b9bcdde3
Riccardo Massidda <contact@rmassidda.it>
2022-04-20 08:49:09 +0200

    Working fast version of ACA

--- aca.py
+++ aca.py
@@ -6,11 +6,18 @@ def adaptive_cross_approximation(
     verbose: bool = False
 ) -> list[int]:
 
+    n, _ = A.shape
+
     # Empty list of indeces
     inducing_points = []
 
     # Residual matrix
     R = A
+
+    # Partial residual matrix
+    rho = torch.zeros((max_iter, n))
+    alpha = torch.zeros((max_iter,))
+    diag = R.diag()
 
     # Check stop criterion
     converged = False  # np.trace(R) <= eps
@@ -18,24 +25,47 @@ def adaptive_cross_approximation(
     # Iteration counter
     iter = 0
 
-    # Verbose
-    # if verbose:
-    #     print(f"Iter {iter:03d} | Trace: {np.trace(R):.3f}")
-
     while not converged and iter < max_iter:
-        i = torch.argmax(torch.abs(R.diag()))
+        # Verbose state
+        if verbose:
+            print(f"Iter {iter:03d}")#:#: | Trace: {np.trace(R):.3f}")
 
-        # Update residual
+        # Select best point
+        i = torch.argmax(torch.abs(diag))
+
+        # Check correctness of the index
+        assert torch.allclose(i, torch.argmax(torch.abs(R.diag())))
+
+        # Update partial residual
+        rho[iter] = A[:, i]
+        for j in range(iter):
+            rho[iter] = rho[iter] - rho[j, i] * rho[j] / alpha[j]
+        alpha[iter] = rho[iter, i]
+
+        # Check correctness of the residual
+        print("rho:", rho[iter])
+        print("alpha:", alpha[iter])
+        print("rho_gt:", R[:, i])
+        print("alpha_gt:", R[i, i])
+        assert torch.allclose(rho[iter], R[:, i])
+        assert torch.allclose(alpha[iter], R[i, i])
+
+        # Update diagonal
+        for j in range(n):
+            diag[j] = diag[j] - torch.square(rho[iter, j]) / alpha[iter]
+
+        # Update residual (explicit)
         R = R - torch.outer(
             R[:, i] / R[i, i],
             R[i, :])
 
+        # Check correctness of the diagonal
+        print("diag:", diag)
+        print("diag_gt:", R.diag())
+        assert torch.allclose(diag, R.diag())
+
         # Append example
         inducing_points.append(i)
-
-        # Verbose state
-        if verbose:
-            print(f"Iter {iter+1:03d}")#:#: | Trace: {np.trace(R):.3f}")
 
         # Update convergence
         converged = False  # np.trace(R) <= eps
@@ -44,3 +74,19 @@ def adaptive_cross_approximation(
         iter += 1
 
     return inducing_points
+
+
+def main(n: int = 5, m: int = 4):
+    # Generate data
+    A = torch.randn(n, n)
+    A = A.t() @ A
+
+    # Compute ACA
+    inducing_points = adaptive_cross_approximation(A, max_iter=3, verbose=True)
+
+    # Print results
+    print(f"Inducing points: {inducing_points}")
+
+
+if __name__ == "__main__":
+    main()
